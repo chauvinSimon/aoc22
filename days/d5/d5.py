8ad19b12202aa5358adf87bf692bb54aa91f2b05
Simon Chauvin <simon.chauvin@esrlabs.com>
2022-12-05 14:26:58 +0100

    feature - add d5

---
+++ days/d5/d5.py
@@ -0,0 +1,62 @@
+import re
+from collections import defaultdict
+from pathlib import Path
+
+from utils.utils import read_file
+
+
+def move(state, moves, use_block: bool = False) -> bool:
+    for mov in moves:
+        m = re.match(
+            r"move (?P<n_moves>\d+) from (?P<i_origin>\d+) to (?P<i_target>\d+)",
+            mov
+        )
+        res = dict(m.groupdict())
+        n_moves = int(res['n_moves'])
+        i_origin = int(res['i_origin']) - 1
+        i_target = int(res['i_target']) - 1
+        tmp = []
+        for _ in range(n_moves):
+            tmp.append(state[i_origin][0])
+            if len(state[i_origin]) == 1:
+                state[i_origin] = []
+            else:
+                state[i_origin] = state[i_origin][1:]
+        if not use_block:
+            tmp = tmp[::-1]
+        state[i_target] = tmp + state[i_target]
+
+    return state
+
+
+def decode_state(state_str):
+    n_piles = int(state_str[-1][-1])  # index starting with 1
+    state = [[] for _ in range(n_piles)]
+    for s in state_str[:-1]:
+        for i_pile in range(n_piles):
+            _id = 1 + 4 * i_pile
+            if len(s) > _id:
+                letter = s[_id]
+                if letter == ' ':
+                    continue
+                state[i_pile].append(letter)
+    return state
+
+
+def answer(state) -> str:
+    return ''.join((s[0] for s in state))
+
+
+def main():
+    for i, use_block in enumerate([False, True]):
+        lines = read_file(Path('days/d5/in.txt'))
+        i_split = lines.index('')
+        state_str = lines[:i_split]
+        moves = lines[1 + i_split:]
+        state = decode_state(state_str)
+
+        print(f'answer {i}: {answer(move(state=state, moves=moves, use_block=use_block))}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ days/d5/test_d5.py
@@ -0,0 +1,68 @@
+import pytest
+
+from days.d5.d5 import decode_state, move
+
+
+@pytest.mark.parametrize("state_str, state_expected", [
+    ([
+         '    [D]    ',
+         '[N] [C]    ',
+         '[Z] [M] [P]',
+         ' 1   2   3',
+     ],
+     [
+         ['N', 'Z'],
+         ['D', 'C', 'M'],
+         ['P'],
+     ]),
+])
+def test_state_decoding(state_str, state_expected):
+    state = decode_state(state_str)
+    assert state_expected == state
+
+
+@pytest.mark.parametrize("state, moves, end_state_expected", [
+    ([
+         ['N', 'Z'],
+         ['D', 'C', 'M'],
+         ['P'],
+     ], [
+         'move 1 from 2 to 1',
+         'move 3 from 1 to 3',
+         'move 2 from 2 to 1',
+         'move 1 from 1 to 2',
+     ], [
+         ['C'],
+         ['M'],
+         ['Z', 'N', 'D', 'P'],
+     ]
+    ),
+])
+def test_moves(state, moves, end_state_expected):
+    end_state = move(state, moves)
+    assert end_state_expected == end_state
+
+@pytest.mark.parametrize("state, moves, end_state_expected", [
+    ([
+         ['N', 'Z'],
+         ['D', 'C', 'M'],
+         ['P'],
+     ], [
+         'move 1 from 2 to 1',
+         'move 3 from 1 to 3',
+         'move 2 from 2 to 1',
+         'move 1 from 1 to 2',
+     ], [
+         ['M'],
+         ['C'],
+         ['D', 'N', 'Z', 'P'],
+     ]
+    ),
+])
+def test_moves_block(state, moves, end_state_expected):
+    end_state = move(state, moves, use_block=True)
+    assert end_state_expected == end_state
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])

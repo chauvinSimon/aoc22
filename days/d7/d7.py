e5f8ec67e6063c07bd0b4cc73e2fc8d3d3ba441a
Simon Chauvin <simon.chauvin@esrlabs.com>
2022-12-07 11:39:29 +0100

    feature - add d7 - not working

---
+++ days/d7/d7.py
@@ -0,0 +1,99 @@
+import re
+from functools import reduce
+from pathlib import Path
+from typing import Dict, List
+
+from utils.utils import read_file
+
+
+def deep_get(dictionary, *keys):
+    return reduce(
+        lambda d, key: d.get(key)
+        if d else None,
+        keys,
+        dictionary
+    )
+
+
+def find_tree(terminal: List[str]) -> Dict:
+    tree = {'/': {}}
+    i = 0
+    current_path = []
+    while i < len(terminal):
+        line = terminal[i]
+        if '$ cd ' in line:
+            new_folder = line[5:]
+            if new_folder == '/':
+                current_path = [new_folder]
+            elif new_folder == '..':
+                current_path = current_path[:-1]
+            else:
+                current_path.append(new_folder)
+            # print(f'now in [{current_path}]')
+            i = i + 1
+        elif '$ ls' == line:
+            while i < len(terminal) - 1:
+                i = i + 1
+                line = terminal[i]
+                if line[0] == '$':
+                    break
+                # print(f'in {current_path}: {line}')
+                if 'dir ' in line:
+                    name = line[4:]
+                    if name not in deep_get(tree, *current_path):
+                        deep_get(tree, *current_path)[name] = {}
+                else:
+                    m = re.match(r"(?P<size>\d+) (?P<name>\w.*)", line)
+                    match = m.groupdict()
+                    # print(f"{match['name']}: {match['size']}")
+                    deep_get(tree, *current_path)[match['name']] = int(match['size'])
+        else:
+            i = i + 1
+
+    # import json
+    # print(json.dumps(tree, sort_keys=True, indent=4))
+
+    return tree
+
+
+def find_dir_size(_d) -> int:
+    if isinstance(_d, int):
+        return _d
+    return sum(find_dir_size(v) for v in _d.values())
+
+
+def find_dir_sizes(tree):
+    sizes = {}
+
+    def examine_tree(_tree):
+        for k, v in _tree.items():
+            if isinstance(v, dict):
+                dir_size = find_dir_size(v)
+                # print(f'dir_size of [{k}] = {dir_size}')
+                sizes[k] = dir_size
+                examine_tree(v)
+
+    examine_tree(tree)
+    # print(sizes)
+    return sizes
+
+
+def answer(dir_and_sizes) -> int:
+    my_sum = 0
+    for k, v in dir_and_sizes.items():
+        if v < 100000:
+            # print(f'{k}: {v}')
+
+            my_sum += v
+    return sum(s for s in dir_and_sizes.values() if s <= 100000)
+
+
+def main():
+    terminal = read_file(Path('days/d7/in.txt'))
+    tree = find_tree(terminal=terminal)
+    dir_sizes = find_dir_sizes(tree)
+    print(f'answer: {answer(dir_sizes)}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ days/d7/test_d7.py
@@ -0,0 +1,92 @@
+import pytest
+
+from days.d7.d7 import find_dir_sizes, find_tree
+
+
+@pytest.mark.parametrize("terminal, tree_expected", [
+    ([
+         '$ cd /',
+         '$ ls',
+         'dir a',
+         '14848514 b.txt',
+         '8504156 c.dat',
+         'dir d',
+         '$ cd a',
+         '$ ls',
+         'dir e',
+         '29116 f',
+         '2557 g',
+         '62596 h.lst',
+         '$ cd e',
+         '$ ls',
+         '584 i',
+         '$ cd ..',
+         '$ cd ..',
+         '$ cd d',
+         '$ ls',
+         '4060174 j',
+         '8033020 d.log',
+         '5626152 d.ext',
+         '7214296 k',
+     ],
+     {
+         '/': {
+             'a': {
+                 'e': {
+                     'i': 584
+                 },
+                 'f': 29116,
+                 'g': 2557,
+                 'h.lst': 62596,
+             },
+             'b.txt': 14848514,
+             'c.dat': 8504156,
+             'd': {
+                 'j': 4060174,
+                 'd.log': 8033020,
+                 'd.ext': 5626152,
+                 'k': 7214296,
+             },
+         }
+     }),
+])
+def test_find_tree(terminal, tree_expected):
+    tree = find_tree(terminal)
+    assert tree_expected == tree
+
+
+@pytest.mark.parametrize("tree, dir_and_size_expected", [
+    ({
+         '/': {
+             'a': {
+                 'e': {
+                     'i': 584
+                 },
+                 'f': 29116,
+                 'g': 2557,
+                 'h.lst': 62596,
+             },
+             'b.txt': 14848514,
+             'c.dat': 8504156,
+             'd': {
+                 'j': 4060174,
+                 'd.log': 8033020,
+                 'd.ext': 5626152,
+                 'k': 7214296,
+             },
+         }
+     },
+     {
+         'a': 94853,
+         'e': 584,
+         'd': 24933642,
+         '/': 48381165
+     })
+])
+def test_find_dir_lighter(tree, dir_and_size_expected):
+    dir_and_size = find_dir_sizes(tree)
+    assert dir_and_size_expected == dir_and_size
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
